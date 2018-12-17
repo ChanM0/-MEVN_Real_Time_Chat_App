99958abaaecfac767e5de5c266ea842f4f05bb8a
chanM0 <mikec201722@gmail.com>
2018-12-16 23:37:34 -0800

    Able to send a message

--- Express-Mongo-Back-end/index.js
+++ Express-Mongo-Back-end/index.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 
+var moment = require("moment");
+moment().format("LLL");
+
 app.use(cors());
 app.use(bodyParser.json()); // accept JSON data
 app.use(bodyParser.urlencoded({ extended: true })); // accept form data
@@ -19,19 +22,46 @@ const url = "mongodb://localhost:27017";
 const userDatabase = "userDatabase";
 const chatMessageDatabase = "chatMessageDatabase";
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
 
 var userFound = null;
 
 const findUser = function(db, query, callback) {
   const collection = db.collection("documents");
+  console.log(query);
   collection.find(query).toArray(function(err, docs) {
     assert.equal(err, null);
     console.log("Found the following records: " + docs.length);
-    console.log(docs);
+    // console.log(docs);
     userFound = docs;
     callback(docs);
   });
+};
+const insertMessage = function(db, data, callback) {
+  const collection = db.collection("documents");
+  console.log(data);
+
+  collection.insertMany(data, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.ops.length);
+    console.log("Inserted 1 message into the collection");
+    callback(result);
+  });
+};
+
+const insertUser = function(db, data, callback) {
+  // Get the documents collection
+  const collection = db.collection("documents");
+  // Insert some documents
+  collection.insertMany(
+    [{ username: data.username, password: data.password }],
+    function(err, result) {
+      assert.equal(err, null);
+      assert.equal(1, result.ops.length);
+      console.log("Inserted 1 message into the collection");
+      callback(result);
+    }
+  );
 };
 
 app.post("/api/create/user", function(req, res) {
@@ -54,22 +84,6 @@ app.post("/api/create/user", function(req, res) {
         let query = { username: data.username };
         findUser(db, query, function() {
           if (userFound.length == 0) {
-            // create new user
-            const insertUser = function(db, data, callback) {
-              // Get the documents collection
-              const collection = db.collection("documents");
-              // Insert some documents
-              collection.insertMany(
-                [{ username: data.username, password: data.password }],
-                function(err, result) {
-                  assert.equal(err, null);
-                  assert.equal(1, result.ops.length);
-                  console.log("Inserted 1 message into the collection");
-                  callback(result);
-                }
-              );
-            };
-
             // insertUser
             insertUser(db, data, function() {
               res.status(200).send(username);
@@ -102,7 +116,9 @@ app.post("/api/login/user", function(req, res) {
         console.log("Connected successfully to server");
 
         const db = client.db(userDatabase);
+
         let query = { username: data.username };
+
         findUser(db, query, function() {
           if (userFound.length != 0) {
             // create new user
@@ -138,12 +154,91 @@ app.get("/api/all/users", function(req, res) {
         if (userFound.length != 0) {
           res.status(200).send(userFound);
         } else {
-          res.status(401).send("User does not exist.");
+          res.status(401).send("Invalid Credentials.");
         }
         client.close();
       });
     }
   );
 });
+
+// io.on("connection", function(socket) {
+//Creates a new message
+app.post("/api/chat/new/message", function(req, res) {
+  // use socket .io here
+  let sender = req.body.sender;
+  let receiver = req.body.receiver;
+  let message = req.body.message;
+  let timeStamp = moment(new Date());
+
+  let senderFound = null;
+  let receiverFound = null;
+
+  let data = [
+    {
+      sender: sender,
+      receiver: receiver,
+      message: message,
+      timeStamp: timeStamp
+    }
+  ];
+
+  console.log(data);
+
+  userFound = null;
+
+  if (sender != null && receiver != null && message.length != 0) {
+    MongoClient.connect(
+      url,
+      function(err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+
+        const dbChat = client.db(chatMessageDatabase);
+        const dbUser = client.db(userDatabase);
+
+        let query = { username: receiver };
+
+        findUser(dbUser, query, function() {
+          receiverFound = userFound;
+
+          if (receiverFound.length != 0) {
+            console.log(receiver);
+            userFound = null;
+
+            insertMessage(dbChat, data, function() {
+              res.status(200).send("success");
+              /**
+               * Socket.io
+               */
+              client.close();
+            });
+          } else {
+            res.status(401).send("Invalid user.");
+            client.close();
+          }
+        });
+      }
+    );
+  } else {
+    res.status(401).send("Invalid Credentials.");
+  }
+});
+
+//get this users last 50 messages
+app.get("api/chat/50/:username", function() {
+  res.status(200).send("Success");
+});
+
+// Get this users sent and received messages
+app.get("api/chat/sent/received/:username", function() {
+  res.status(200).send("Success");
+});
+
+// Start a new conversation
+app.get("api/chat/:usernameSender/:usernameReceiver", function() {
+  res.status(200).send("Success");
+});
+// });
 
 http.listen(3000);
