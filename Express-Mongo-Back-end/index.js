9ccd586fac8b1f3c351956ee2b69cd487bb870a0
chanM0 <mikec201722@gmail.com>
2018-12-16 16:00:03 -0800

    Able to register login and get all users

--- Express-Mongo-Back-end/index.js
+++ Express-Mongo-Back-end/index.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const cors = require('cors');
-
+const cors = require("cors");
 
 app.use(cors());
 app.use(bodyParser.json()); // accept JSON data
 app.use(bodyParser.urlencoded({ extended: true })); // accept form data
 
-
 // use socket.io
-const http = require('http').createServer(app);  
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
@@ -21,121 +19,131 @@ const url = "mongodb://localhost:27017";
 const userDatabase = "userDatabase";
 const chatMessageDatabase = "chatMessageDatabase";
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
+app.get("/", (req, res) => res.send("Hello World!"));
 
 var userFound = null;
 
-app.post('/api/create/user',function(req,res){
-    console.log(req.body);
-    username = req.body.username;
-    password = req.body.password;
+const findUser = function(db, query, callback) {
+  const collection = db.collection("documents");
+  collection.find(query).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records: " + docs.length);
+    console.log(docs);
+    userFound = docs;
+    callback(docs);
+  });
+};
 
-    userFound = null
+app.post("/api/create/user", function(req, res) {
+  console.log(req.body);
+  username = req.body.username;
+  password = req.body.password;
 
-    data = {username : username, password: password};
-
-    const findUser = function(db, data, callback) {
-        const collection = db.collection("documents");
-        collection.find( { username : data.username } ).toArray(function(err, docs) {
-          assert.equal(err, null);
-          console.log("Found the following records: " + docs.length);
-          console.log(docs);
-          userFound = docs
-          callback(docs);
-        });
-    };
-
-    MongoClient.connect(url,function(err, client) {
+  userFound = null;
+  data = { username: username, password: password };
+  if (data.username.length == 0 || data.password.length == 0) {
+    res.status(401).send("Empty input is invalid.");
+  } else {
+    MongoClient.connect(
+      url,
+      function(err, client) {
         assert.equal(null, err);
         console.log("Connected successfully to server");
-        const db = client.db(userDatabase);
 
-        findUser(db,data,function(){
-            if(userFound.length == 0){
-                const insertUser = function(db,data, callback) {
-                    let username = data.username;
-                    let password = data.password;
-                    const collection = db.collection('documents');
-                    collection.insertMany([
-                        {
-                            username: username,
-                            password: password
-                        }
-                    ], function(err, result) {
-                        console.log(err);
-                        assert.equal(err, null);
-                        assert.equal(1, result.ops.length);
-                        console.log("Inserted 1 message into the collection");
-                        callback(result);
-                    });
+        const db = client.db(userDatabase);
+        let query = { username: data.username };
+        findUser(db, query, function() {
+          if (userFound.length == 0) {
+            // create new user
+            const insertUser = function(db, data, callback) {
+              // Get the documents collection
+              const collection = db.collection("documents");
+              // Insert some documents
+              collection.insertMany(
+                [{ username: data.username, password: data.password }],
+                function(err, result) {
+                  assert.equal(err, null);
+                  assert.equal(1, result.ops.length);
+                  console.log("Inserted 1 message into the collection");
+                  callback(result);
                 }
-                console.log("No User found "+userFound)
-              if(data.username.length !=0 && data.password.length !=0 ){
-                  console.log("Data is Valid: "+ data);
-                  insertUser(db,data,function(){
-                      res.status(200).send( username );
-                      client.close();
-                  })
-              }
-            else{
-                res.status(401).send( 'Input is invalid.' );
-                client.close();
-            }
+              );
+            };
+
+            // insertUser
+            insertUser(db, data, function() {
+              res.status(200).send(username);
+            });
+          } else {
+            res.status(401).send("User already exists.");
           }
-        // If user is found 
-        else{
-          res.status(400).send('User already exists!');
           client.close();
-         }
         });
-        
-        client.close();
-    });
-    // res.send("Hello World!")
+      }
+    );
+  }
 });
 
+app.post("/api/login/user", function(req, res) {
+  console.log(req.body);
+  username = req.body.username;
+  password = req.body.password;
 
-// app.post('/api/create/user',function(req,res){
-//   console.log(req.body);
-//   username = req.body.username;
-//   password = req.body.username;
+  userFound = null;
+  data = { username: username, password: password };
 
-//   userFound = null
+  if (data.username.length == 0 || data.password.length == 0) {
+    res.status(401).send("Empty input is invalid.");
+  } else {
+    MongoClient.connect(
+      url,
+      function(err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
 
-//   data = {username : username, password: password};
-  // Use connect method to connect to the server
-//   MongoClient.connect(url,function(err, client) {
-//     assert.equal(null, err);
-//     console.log("Connected successfully to server");
-    
-//     const db = client.db(userDatabase);
-    
-//     findUser(db,data,function(){
-//         // If no user is found 
-//         if(userFound.length == 0){
-//               console.log("No User found "+userFound)
-//             if(data.username.length !=0 && data.password.length !=0 ){
-//                 console.log("Data is Valid: "+ data);
-//                 insertUser(db,data,function(){
-//                     res.status(200).send( username );
-//                 })
-//             }
-//           else{
-//               res.status(401).send( 'Input is invalid.' );
-//           }
-//         }
-//       // If user is found 
-//       else{
-//         res.status(400).send('User already exists!');
-//       }
-//     });
-//     client.close();
-//   });
+        const db = client.db(userDatabase);
+        let query = { username: data.username };
+        findUser(db, query, function() {
+          if (userFound.length != 0) {
+            // create new user
+            if (
+              userFound[0].username == username &&
+              userFound[0].password == password
+            ) {
+              res.status(200).send(userFound[0].username);
+            } else {
+              res.status(401).send("Invalid Credentials.");
+            }
+          } else {
+            res.status(401).send("User does not exist.");
+          }
+          client.close();
+        });
+      }
+    );
+  }
+});
 
+app.get("/api/all/users", function(req, res) {
+  MongoClient.connect(
+    url,
+    function(err, client) {
+      assert.equal(null, err);
+      console.log("Connected successfully to server");
 
-//   res.send("Hello World!")
+      const db = client.db(userDatabase);
 
-// })
+      let query = {};
+      findUser(db, query, function() {
+        if (userFound.length != 0) {
+          res.status(200).send(userFound);
+        } else {
+          res.status(401).send("User does not exist.");
+        }
+        client.close();
+      });
+    }
+  );
+});
 
 http.listen(3000);
